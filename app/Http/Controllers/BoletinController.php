8e05401dbc386af092958c26dea090c22f8bd731
Andrés González <gandresto@outlook.com>
2019-12-09 02:28:48 -0600

    Controladores vacíos para boletín y sumario

---
+++ app/Http/Controllers/BoletinController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Boletin;
+use Illuminate\Http\Request;
+
+class BoletinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Boletin  $boletin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Boletin $boletin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Boletin  $boletin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Boletin $boletin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Boletin  $boletin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Boletin $boletin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Boletin  $boletin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Boletin $boletin)
+    {
+        //
+    }
+}
