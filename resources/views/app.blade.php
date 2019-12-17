01c71bd6ef3701ed57173949df577b12d6a4b92b
Andrés González <gandresto@outlook.com>
2019-12-16 21:45:33 -0600

    Reestructuro las vistas en php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Boletines</title>
+    <title>Web Boletines</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
