b8f94c6143ad8ec029e34c9683871f8590755b20
Andrés González <gandresto@outlook.com>
2019-12-16 21:38:28 -0600

    Redirijo toda ruta a index

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('{any}', function () {
     return view('index');
-});
-
-// Route::resource('boletines', 'BoletinController');
+})->where('any', '.*');
