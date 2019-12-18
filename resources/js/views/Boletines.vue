646af672b0e9c2d15a4916f18425457e71a2c5bd
Andrés González <gandresto@outlook.com>
2019-12-17 21:00:10 -0600

    Agrego tabla con datos de boletines

--- resources/js/views/Boletines.vue
+++ resources/js/views/Boletines.vue
@@ -10,15 +10,62 @@
             </div>
         </div>
         <div class="col-sm-12 col-md-9">
-            <b-table></b-table>
+            <div :v-if="estadoApi == ESTADO_API.LISTO">
+                <b-pagination
+                    :v-model="meta.current_page"
+                    :total-rows="rows"
+                    :per-page="meta.per_page"
+                    aria-controls="tabla-boletines"
+                ></b-pagination>
+                <b-table
+                    id="tabla-boletines"
+                    :items="boletines"
+                    :per_page="meta.per_page"
+                    :current-page="meta.current_page"
+                    :fields="['candidato', 'fecha', 'estado', 'encabezado']">
+                </b-table>
+            </div>
+            <!-- <b-pagination-nav :link-gen="linkGen" :number-of-pages="meta.last_page"></b-pagination-nav> -->
         </div>
     </div>
 </template>
 
 <script>
+    import { mapActions } from 'vuex'
+    import { mapState } from 'vuex'
+    import ESTADO_API from '../enum-estado-api'
+
     export default {
         mounted() {
-            console.log('Component Boletines Montado.')
-        }
+            console.log('Component Boletines Montado.');
+            // this.leerBoletines();
+        },
+        data() {
+            return {
+                ESTADO_API
+            }
+        },
+        methods: {
+            linkGen(pageNum) {
+                return pageNum === 1 ? '?' : `?page=${pageNum}`
+            },
+            ...mapActions([
+                'leerBoletines',
+            ])
+        },
+        computed: {
+            ...mapState({
+                boletines: state => state.boletines.data,
+                links: state => state.boletines.links,
+                meta: state => state.boletines.meta,
+                estadoApi: state => state.estadoApi,
+            }),
+            rows(){
+                return this.boletines.length
+            },
+        },
+        created() {
+            this.leerBoletines();
+        },
     }
 </script>
