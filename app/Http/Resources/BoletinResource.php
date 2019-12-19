d043ca623fc44590b9a6785c8d72bb849a571e4c
Andrés González <gandresto@outlook.com>
2019-12-19 02:54:08 -0600

    Modifico BoletinResource para mostrar más información

--- app/Http/Resources/BoletinResource.php
+++ app/Http/Resources/BoletinResource.php
@@ -14,6 +14,11 @@ class BoletinResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return array_merge(
+            parent::toArray($request),
+            [
+                'sumarios' => SumarioResource::collection($this->sumarios),
+                'categorias' => new CategoriasCollection($this->categorias),
+            ]);
     }
 }
