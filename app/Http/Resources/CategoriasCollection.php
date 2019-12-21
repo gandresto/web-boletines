af27a9c834e376a967a59cacef87afcd231a3cd5
Andrés González <gandresto@outlook.com>
2019-12-20 20:24:40 -0600

    Remapeo las categorias al pedir json

--- app/Http/Resources/CategoriasCollection.php
+++ app/Http/Resources/CategoriasCollection.php
@@ -14,6 +14,37 @@ class CategoriasCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // Definir funciones
+        $esPerio = function($cat){
+            return $cat['tipo'] == 'periodística';
+        };
+        $esPersua = function($cat){
+            return $cat['tipo'] == 'persuasión';
+        };
+        $quitarTipo = function($cat){
+            return array(
+                'id' => $cat['id'],
+                'nombre' => $cat['nombre'],
+            );
+        };
+
+        // Filtrar datos
+        $arr = parent::toArray($request);
+        $persuasivas = array_filter($arr, $esPersua);
+        $periodisticas = array_filter($arr, $esPerio);
+
+        // Remapear arreglo
+        return array(
+            'persuasivas' => array_map($quitarTipo, $persuasivas),
+            'periodisticas' => array_map($quitarTipo, $periodisticas),
+        );
+        // return parent::toArray($request);
+        // return array_map(function($categoria){
+        //     return array(
+        //         'id' => $categoria['id'],
+        //         'categoria' => $categoria['nombre'],
+        //         'tipo' => $categoria['tipo'],
+        //     );
+        // }, parent::toArray($request));
     }
 }
